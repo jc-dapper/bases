58097381b7c5aa0ff0f01e283d35f8366a23ade1
Juan Carlos Sanchez <j.sanchez@dappertechnologies.com>
2022-04-25 14:36:00 -0500

    After hw done correctly

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -19,21 +19,8 @@ export class MainPageComponent {
     }
   ];
 
-   nuevo : Personaje = {
-     nombre: '',
-     poder: 0
-   }
-
-  
-  agregar(){
-    if (this.nuevo.nombre.trim().length === 0)
-      return;
-
-      this.personajes.push(this.nuevo);
-      this.nuevo = {
-        nombre: '',
-        poder : 0
-      }
-    console.log(this.nuevo);
+  nuevo : Personaje = {
+    nombre: 'MR',
+    poder: 10
   }
 }
